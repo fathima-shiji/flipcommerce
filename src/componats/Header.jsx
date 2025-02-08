a04adfbff9732d1ee0e2236e13b400a42aa3a0cd
Shiji <fathimashiji123@gmail.com>
2025-02-09 01:30:47 +0530

    add header component

---
+++ src/componats/Header.jsx
@@ -0,0 +1,16 @@
+import "./header.css"
+
+const Header = () => {
+    return (
+        <header className="main-header">
+            <div className="container">
+                <div className="logo">
+                    <a href="/">FLIXX</a>
+                </div>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
