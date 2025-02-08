4ea0c83232e6a54b0f2dac371b187ac02103eb43
Shiji <fathimashiji123@gmail.com>
2025-02-08 18:09:37 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn mora
+        Click on the Vite and React logos to learn
       </p>
     </>
   )
